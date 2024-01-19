8e5223ef7901bf544957213b2d6681eea63f8f2b
armin-164 <arminsadr164@hotmail.com>
2024-01-19 18:28:55 +0100

    Add library data and GET
    Add an array which contains book data, also
    add a GET which will create a json file at the './library' endpoint
    which is in app.js

---
+++ routes/library.js
@@ -0,0 +1,59 @@
+var express = require("express");
+var router = express.Router();
+
+let bookArray = [
+  {
+    author: "J.K. Rowling",
+    pages: 336,
+    title: "Harry Potter and the Sorcerer's Stone",
+    isLoaned: false,
+  },
+  {
+    author: "George R.R. Martin",
+    pages: 694,
+    title: "A Game of Thrones",
+    isLoaned: false,
+  },
+  {
+    author: "Harper Lee",
+    pages: 324,
+    title: "To Kill a Mockingbird",
+    isLoaned: false,
+  },
+  {
+    author: "J.R.R. Tolkien",
+    pages: 1178,
+    title: "The Lord of the Rings",
+    isLoaned: false,
+  },
+  {
+    author: "Agatha Christie",
+    pages: 272,
+    title: "Murder on the Orient Express",
+    isLoaned: false,
+  },
+  {
+    author: "F. Scott Fitzgerald",
+    pages: 180,
+    title: "The Great Gatsby",
+    isLoaned: false,
+  },
+  {
+    author: "Gabriel García Márquez",
+    pages: 368,
+    title: "One Hundred Years of Solitude",
+    isLoaned: false,
+  },
+  {
+    author: "Mark Twain",
+    pages: 224,
+    title: "The Adventures of Tom Sawyer",
+    isLoaned: false,
+  },
+];
+
+router.get("/", function (req, res) {
+    res.json(bookArray);
+});
+
+module.exports = router;
