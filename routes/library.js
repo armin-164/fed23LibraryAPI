86b90c95948b82084cdfa3b03a4ffa3ca3af1ca6
armin-164 <arminsadr164@hotmail.com>
2024-01-22 18:15:20 +0100

    Add randomUUID to create random generated ID for each book and a GET that will json the book if IDs match

--- routes/library.js
+++ routes/library.js
@@ -1,4 +1,5 @@
 var express = require("express");
+let {randomUUID} = require("crypto");
 var router = express.Router();
 
 let bookArray = [
@@ -7,48 +8,56 @@ let bookArray = [
     pages: 336,
     title: "Harry Potter and the Sorcerer's Stone",
     isLoaned: false,
+    id: randomUUID()
   },
   {
     author: "George R.R. Martin",
     pages: 694,
     title: "A Game of Thrones",
     isLoaned: false,
+    id: randomUUID()
   },
   {
     author: "Harper Lee",
     pages: 324,
     title: "To Kill a Mockingbird",
     isLoaned: false,
+    id: randomUUID()
   },
   {
     author: "J.R.R. Tolkien",
     pages: 1178,
     title: "The Lord of the Rings",
     isLoaned: false,
+    id: randomUUID()
   },
   {
     author: "Agatha Christie",
     pages: 272,
     title: "Murder on the Orient Express",
     isLoaned: false,
+    id: randomUUID()
   },
   {
     author: "F. Scott Fitzgerald",
     pages: 180,
     title: "The Great Gatsby",
     isLoaned: false,
+    id: randomUUID()
   },
   {
     author: "Gabriel García Márquez",
     pages: 368,
     title: "One Hundred Years of Solitude",
     isLoaned: false,
+    id: randomUUID()
   },
   {
     author: "Mark Twain",
     pages: 224,
     title: "The Adventures of Tom Sawyer",
     isLoaned: false,
+    id: randomUUID()
   },
 ];
 
@@ -64,5 +73,14 @@ router.post('/', function(req, res) {
     isLoaned: req.body.isLoaned
   }
   bookArray.push(newBook);
+})
+
+router.get('/:bookId', function(req, res) {
+  let id = req.params.bookId;
+  let book = bookArray.find(book => book.id == id);
+
+  if (book) {
+    res.json(book)
+  }
 })
 module.exports = router;
