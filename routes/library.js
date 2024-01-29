5683a93bfed83e77d33e4961cef5388259ea77a7
armin-164 <arminsadr164@hotmail.com>
2024-01-29 21:11:21 +0100

    Remove book array and put in books.json

--- routes/library.js
+++ routes/library.js
@@ -1,65 +1,6 @@
 var express = require("express");
-let {randomUUID} = require("crypto");
 var router = express.Router();
 
-let bookArray = [
-  {
-    author: "J.K. Rowling",
-    pages: 336,
-    title: "Harry Potter and the Sorcerer's Stone",
-    isLoaned: false,
-    id: randomUUID()
-  },
-  {
-    author: "George R.R. Martin",
-    pages: 694,
-    title: "A Game of Thrones",
-    isLoaned: false,
-    id: randomUUID()
-  },
-  {
-    author: "Harper Lee",
-    pages: 324,
-    title: "To Kill a Mockingbird",
-    isLoaned: false,
-    id: randomUUID()
-  },
-  {
-    author: "J.R.R. Tolkien",
-    pages: 1178,
-    title: "The Lord of the Rings",
-    isLoaned: false,
-    id: randomUUID()
-  },
-  {
-    author: "Agatha Christie",
-    pages: 272,
-    title: "Murder on the Orient Express",
-    isLoaned: false,
-    id: randomUUID()
-  },
-  {
-    author: "F. Scott Fitzgerald",
-    pages: 180,
-    title: "The Great Gatsby",
-    isLoaned: false,
-    id: randomUUID()
-  },
-  {
-    author: "Gabriel García Márquez",
-    pages: 368,
-    title: "One Hundred Years of Solitude",
-    isLoaned: false,
-    id: randomUUID()
-  },
-  {
-    author: "Mark Twain",
-    pages: 224,
-    title: "The Adventures of Tom Sawyer",
-    isLoaned: false,
-    id: randomUUID()
-  },
-];
 
 router.get("/", function (req, res) {
     res.json(bookArray);
